b0003afd16a494f3054fa2167dd4e744d5dae39d
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-09-21 22:55:52 +0900

    変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'homes/top'
-  get 'posts/index'
+  root to: 'homes#top'
+  resources :posts, only: [:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
