889eef4ce89ddf548db904a4212e174f7d5f08ca
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-10-30 00:03:35 +0900

    変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  get 'relationships/followings'
+  get 'relationships/followers'
   devise_for :users
   root to: 'homes#top'
   resources :posts, only: [:index, :new, :create, :edit, :update, :destroy]
+    # ネストさせる
+  resources :users do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
