dfec5c7e02e4a5ed5ede6bfe452096d04bdc1de9
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-11-20 18:57:58 +0900

    変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,4 +25,14 @@ class User < ApplicationRecord
   def following?(user)
     followings.include?(user)
   end
+
+  has_one_attached :profile_image
+  
+  def get_profile_image
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize_to_limit: [100, 100]).processed
+  end
 end
