ef7e88d9a8061dd57dc8f09a9a71cbb71b9dd548
AkihiroTanaka3 <126536571+AkihiroTanaka3@users.noreply.github.com>
2023-11-30 21:59:35 +0900

    変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'relationships/followings'
-  get 'relationships/followers'
   devise_for :users
   root to: 'homes#top'
   resources :posts, only: [:index, :new, :create, :edit, :update, :destroy]
     # ネストさせる
-  resources :users do
+  resources :users, only: [:show, :edit] do
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
